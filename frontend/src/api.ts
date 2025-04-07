341f833f0df56853451a1b4cb690ff08a053dce9
ajay980p <ajay980p@gmail.com>
2025-04-08 02:14:03 +0530

    commit changes

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const API_BASE = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api';
 
+console.log("API base url ", API_BASE, import.meta.env.VITE_API_BASE_URL)
+
 export const getTasks = () => axios.get(`${API_BASE}/getTask`);
 export const addTask = (title: string) => axios.post(`${API_BASE}/addTask`, { title });
 export const deleteTask = (id: string) => axios.delete(`${API_BASE}/deleteTask/${id}`);
